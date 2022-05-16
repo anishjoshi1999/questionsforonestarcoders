f484870113329a43c38ab776caa1508e07b9ee0b
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-05-16 09:08:10 +0545

    Probcat

---
+++ PROBCAT.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    x = int(input())
+    if 1 <= x < 100:
+        print("Easy") 
+    elif 100 <= x < 200:
+        print("Medium")
+    elif 200 <= x <= 300:
+        print("Hard")
+
+# 3
+# 50
+# 172
+# 201
